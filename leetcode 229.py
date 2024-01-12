dc9610f17bfd9fd974f9cdbeb80abb431bf6e678
Shavkat <154513520+ShavkatRakhmatov2005@users.noreply.github.com>
2024-01-12 22:17:35 +0500

    Update leetcode 229.py

--- leetcode 229.py
+++ leetcode 229.py
@@ -1,8 +1,17 @@
-n=int(input())
-lst=[3,2,3]
-dct={}
-for i in lst:
-    dct[i]=lst.count(i)
-for i,j in dct.items():
-    if j>len(lst)//3:
-        print(i)
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        lst=[]
+        dct={}
+        for i in range(len(nums)):
+            if (nums[i] in dct):
+                dct[nums[i]]+=1
+            else:
+                dct[nums[i]]=1
+        for j in dct.keys():
+            if dct[j]>(len(nums)/3):
+                lst.append(j)
+
+        return lst
+
+
+      
